95b81c26d4a5ab2352c44535ce67a66f21b7fd04
Jacob Larsen <jacoblarsen.net@gmail.com>
2016-03-23 21:36:35 +0100

    Update get_and_post.sh

--- pi/get_and_post.sh
+++ pi/get_and_post.sh
@@ -6,7 +6,7 @@ echo $DATA
 
 curl \
 -H application-id:567A9D06-AE22-7A5D-FF35-FE0358F16100 \
--H secret-key:EBCD239C-5133-9E47-FF68-68AC06FC0900 \
+-H secret-key: \
 -H Content-Type:application/json \
 -H application-type:REST \
 -X POST \
